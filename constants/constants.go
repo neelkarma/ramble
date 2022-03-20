7107581dd68d92fed76e13bf21f9c21ec34de850
chickensalt <nsdj.sharma@gmail.com>
2022-03-20 23:21:21 +1100

    use markdown format for entries

--- constants/constants.go
+++ constants/constants.go
@@ -3,7 +3,7 @@ package constants
 const ConfigPath string = "./ramble.json"
 const KeyPath string = "./unlocked.key"
 const EntriesPath string = "./entries/"
-const TempFilePath string = "./temp-ramble"
+const TempFilePath string = "./curr-ramble.md"
 const NoDiaryFoundMsg string = "There is no diary in the current directory. Try initializing one with \"ramble init\"."
 const DiaryNotUnlockedMsg string = "The diary isn't unlocked. Unlock it first with \"ramble unlock\""
 const EntryFileTimeFormat string = "2006-01-02T15-04-05Z"
